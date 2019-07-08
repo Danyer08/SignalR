197d427630bb24abb36731a0c15024bb959dd139
Danyer <danjer0826@gmail.com>
2019-07-08 13:45:35 -0400

    Added API and ClientApp

---
+++ SignalRAPI/SignalRAPI/Models/GeoLocation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRAPI.Models
+{
+    public class GeoLocation
+    {
+        public float Latitude { get; set; }
+
+        public float Longitude { get; set; }
+    }
+}
